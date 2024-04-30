ba83b1713429c13eb3547a2ea59b6afe272d5204
Alejandro Gispert <158798324+AlejandroGispert@users.noreply.github.com>
2024-04-30 15:18:43 +0200

    exercise3 reviewed and fixed

--- javascript/javascript1/week2/homework3.js
+++ javascript/javascript1/week2/homework3.js
@@ -2,36 +2,23 @@
 
 const appropiateWearHelper = (temp) => {
   console.log("--we only have support for jackets at the moment--");
-
-  const jacketDrawer = [
-    "Down Jacket",
-    "Insulated Jacket",
-    "Winter Jacket",
-    "Lightweight Jacket",
-    "Sports Jacket",
-    "Lightweight Hoodie",
-    "Fleece Jacket",
-    "Windbreaker",
-    "Leather Jacket",
-    "Parka",
-  ];
-
+  //deleted array
   if (-10 >= temp) {
-    return "you should wear a: " + jacketDrawer[0];
+    return "you should wear a Down Jacket";
   } else if (temp <= 0) {
-    return "you should wear a: " + jacketDrawer[1];
+    return "you should wear an Insulated Jacket";
   } else if (temp <= 10) {
-    return "you should wear a: " + jacketDrawer[9] + " or a " + jacketDrawer[2];
+    return "you should wear a Parka or a Winter Jacket";
   } else if (temp <= 20) {
-    return "you should wear a: " + jacketDrawer[8] + " or a " + jacketDrawer[5];
+    return "you should wear a Leather Jacket or a Lightweight Jacket";
   } else if (temp <= 30) {
-    return "you should wear a: " + jacketDrawer[7];
+    return "you should wear a Sports Jacket";
   } else if (temp <= 40) {
-    return "you should wear a: " + jacketDrawer[4];
+    return "you should wear a Lightweight Hoodie";
   } else if (40 < temp) {
     return "you shouldn't wear anything";
   }
 };
 
-const clothesToWear = appropiateWearHelper(-10);
+const clothesToWear = appropiateWearHelper(41);
 console.log(clothesToWear); // Logs out: "shorts and a t-shirt"
