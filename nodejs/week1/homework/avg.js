eb1966e6aa7281c1af6a4cc31624e796a5059fd3
Alejandro Gispert <158798324+AlejandroGispert@users.noreply.github.com>
2024-08-08 16:05:28 +0200

    added invalid input loop

--- nodejs/week1/homework/avg.js
+++ nodejs/week1/homework/avg.js
@@ -6,6 +6,12 @@
 // console.log(parseInt(args.i) + parseInt(args.n));
 
 const args = process.argv.slice(2);
+for (let i = 0; i < args.length; i++) {
+  if (typeof args[i] === "string") {
+    console.log(`Invalid input: ${args[i]}`);
+    process.exit(1);
+  }
+}
 
 if (args.length === 0) {
   console.log("No numbers provided. Insert a number please.");
@@ -17,6 +23,6 @@ for (let arg of args) {
   sum += Number(arg);
 }
 const numbers = args.map(Number);
-const average = (sum / numbers.length).toFixed(2);
+const average = (sum / numbers.length).toFixed(0);
 // Output the sum
 console.log(`The result is: ${average}`);
