f4a63084a5ad7dc7bfa7d0446e23b626c6f45156
Alejandro Gispert <158798324+AlejandroGispert@users.noreply.github.com>
2024-09-16 17:42:24 +0200

    blog ex started

---
+++ react/react2/inclass/app/blogs/pages.jsx
@@ -0,0 +1,13 @@
+"use client";
+import { useEffect, useState } from "react";
+
+import Image from "next/image";
+// Exercise 2
+
+export default function blog() {
+  return (
+    <>
+      <h3>Hlola</h3>
+    </>
+  );
+}
