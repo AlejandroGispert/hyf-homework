5c2642590870006bb1b79b2ead9e370a75f0b971
Alejandro Gispert <158798324+AlejandroGispert@users.noreply.github.com>
2024-09-21 22:02:45 +0200

    homework finish

---
+++ react/react2/inclass/app/_page.js
@@ -0,0 +1,31 @@
+// pages/_app.js
+import * as React from "react";
+import Head from "next/head";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#1976d2",
+    },
+    secondary: {
+      main: "#dc004e",
+    },
+  },
+});
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <React.Fragment>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstarts an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </React.Fragment>
+  );
+}
